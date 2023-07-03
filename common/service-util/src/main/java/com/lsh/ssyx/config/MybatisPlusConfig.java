68e27bcde28b3eb60c317e57ceee8da34c8f88ab
liushaohua <shaohua.liu@zhaopin.com.cn>
2023-07-03 17:59:47 +0800

    init

---
+++ common/service-util/src/main/java/com/lsh/ssyx/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.lsh.ssyx.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author shaohua.liu
+ * Create at 2023/7/3
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.lsh.ssyx.*.mapper")
+public class MybatisPlusConfig {
+    /**
+     * mp插件
+     */
+    @Bean
+    public MybatisPlusInterceptor optimisticLockerInnerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //向Mybatis过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
